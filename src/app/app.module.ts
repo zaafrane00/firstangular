1da2ed7ae91ce8c18d442780b4d4ac9bf22962b4
hamza zaafrane <zaafranehamza@gmail.com>
2020-12-10 09:51:25 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+import { AboutComponent } from './about/about.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewComponentComponent,
+    AboutComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
